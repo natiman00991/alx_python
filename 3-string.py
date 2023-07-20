fc69dc6dbe916bf3f2f887b53bcfa4a0b1c1f22d
natiman <natiabnew@gmail.com>
2023-07-20 06:01:34 -0700

    suuuuuuuuuuuuuuuuu

---
+++ 3-string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def reverse_string(string):
+    return string[::-1]
+
+
+print(reverse_string("Hello"))
+print(reverse_string(""))
+print(reverse_string("madam"))
+print(reverse_string("Hello, World!"))
