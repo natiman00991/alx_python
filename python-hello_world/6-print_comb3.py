12083dba49c02cfed70542d7286e71ea76b31e3c
natiman <natiabnew@gmail.com>
2023-07-20 03:54:21 -0700

    dsdf

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,5 +2,5 @@ for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
         print(
             "{:02d}".format(tens_digit * 10 + ones_digit),
-            end=", " if ones_digit < 9 else "\n",
+            end=", " if tens_digit < 8 or ones_digit < 9 else "\n",
         )
