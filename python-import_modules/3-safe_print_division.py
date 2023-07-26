55b016381a456d8ea546b5e89392ef64f0763f12
natiman <natiabnew@gmail.com>
2023-07-26 02:51:49 -0700

    hhhhhhhhhhhhhh

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,3 +5,17 @@ def safe_print_division(a, b):
         result = None
     finally:
         print("Inside result: {}".format(result))
+    return result
+
+
+# Test the function
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
