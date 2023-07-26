f4f7a36b4141103cff357cbb1e97a2f44ce7cfae
natiman <natiabnew@gmail.com>
2023-07-26 01:41:08 -0700

    ggg

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,13 @@
-from add_0 import add
+if __name__ == "__main__":
+    # Import the add function from add_0.py
+    from add_0 import add
 
-a = 1
-b = 2
+    # Define variables a and b
+    a = 1
+    b = 2
 
-x = add(a, b)
-print("{} + {} = {}".format(a, b, x))
+    # Call the add function with arguments a and b and store the result in the variable 'result'
+    result = add(a, b)
+
+    # Print the result using string formatting
+    print(f"{a} + {b} = {result}")
