e5dee7841e3d51d4a973011b378cc7f0a14a3c24
natiman <natiabnew@gmail.com>
2023-07-26 11:57:32 -0700

    adsd

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,13 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end=" " if i < len(row) - 1 else "")
+        print()
+
+
+# Test case
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# print_matrix_integer(matrix)
+# print("--")
+# print_matrix_integer()
