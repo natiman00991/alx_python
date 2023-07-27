890e50c40808278ac75afc101935680047faae65
natiman <natiabnew@gmail.com>
2023-07-27 03:05:30 -0700

    net

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,19 +1,3 @@
 def common_elements(set_1, set_2):
-    # Initialize an empty set to store the common elements
-    common_set = set()
-
-    # Loop through each element in set_1
-    for element in set_1:
-        # Check if the element is also in set_2
-        if element in set_2:
-            # Add the common element to the common_set
-            common_set.add(element)
-
-    return common_set
-
-
-# Test the function with the given sets
-# set_1 = { "Python", "C", "Javascript" }
-# set_2 = { "Bash", "C", "Ruby", "Perl" }
-# c_set = common_elements(set_1, set_2)
-# print(sorted(list(c_set)))
+    # Use set intersection to find common elements
+    return set_1 & set_2
