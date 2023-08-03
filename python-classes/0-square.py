34b13fa5da814a94b16bbab8a97b0f991d899ea6
natiman <natiabnew@gmail.com>
2023-08-03 11:18:43 -0700

    hello

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,34 @@
 class Square:
+    """
+    Class Square: defines a square by its size.
+
+    Private instance attribute:
+        __size: Size of the square.
+
+    Instantiation with size (no type/value verification).
+    """
+
     def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
 
 
-# if __name__ == "__main__":
-#     my_square = Square(3)
-#     print(type(my_square))
-#     print(my_square.__dict__)
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
 
-#     try:
-#         print(my_square.size)
-#     except Exception as e:
-#         print(e)
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
 
-#     try:
-#         print(my_square.__size)
-#     except Exception as e:
-#         print(e)
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
