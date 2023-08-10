749fd3079a162bc0471dbfd2e9e80d23a060ff92
natiman <natiabnew@gmail.com>
2023-08-10 11:40:01 -0700

    dddddddddddddd

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,30 @@
+# #!/usr/bin/python3
+# """
+# Fetches the status of a URL using requests package.
+# """
+# import requests
+
+# if __name__ == "__main__":
+#     url = "https://alu-intranet.hbtn.io/status"
+#     response = requests.get(url)
+#     content_type = type(response.text).__name__
+#     content = response.text
+
+#     # print("Body response:")
+#     # print("\t- type:", content_type)
+#     # print("\t- content:", content)
 #!/usr/bin/python3
 """
-Fetches the status of a URL using requests package.
+Fetches the status of a URL using the requests package.
 """
 import requests
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     content_type = type(response.text).__name__
     content = response.text
 
-    # print("Body response:")
-    # print("\t- type:", content_type)
-    # print("\t- content:", content)
+    print("Body response:")
+    print("\t- type:", content_type)
+    print("\t- content:", content)
