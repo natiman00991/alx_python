0c031bd63a6b2d79ac07646cbd5864fb463d0111
natiman <natiabnew@gmail.com>
2023-08-10 10:18:37 -0700

    ffffff

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+This module contains the Rectangle class.
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    The Rectangle class that inherits from Base.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a new instance of the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate. Defaults to 0.
+            y (int, optional): The y-coordinate. Defaults to 0.
+            id (int, optional): The id of the rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y attribute."""
+        self.__y = value
+
+
+# if __name__ == "__main__":
+#     r1 = Rectangle(10, 2)
+#     print(r1.id)
+
+#     r2 = Rectangle(2, 10)
+#     print(r2.id)
+
+#     r3 = Rectangle(10, 2, 0, 0, 12)
+#     print(r3.id)
