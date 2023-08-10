f5f1d9c3bd254aa88907c8a9b0c0cd7e1e71d087
natiman <natiabnew@gmail.com>
2023-08-10 11:34:42 -0700

    aaaaaaaaaaaaaaaa

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,6 +1,21 @@
+# #!/usr/bin/python3
+# """
+# Sends a request to a URL and displays the body of the response or error code.
+# """
+# import requests
+# import sys
+
+# if __name__ == "__main__":
+#     url = sys.argv[1]
+#     response = requests.get(url)
+
+#     if response.status_code >= 400:
+#         print("Error code:", response.status_code)
+#     else:
+#         print(response.text)
 #!/usr/bin/python3
 """
-Sends a request to a URL and displays the response body or error code.
+Sends a request to a URL and displays the body of the response or error code.
 """
 import requests
 import sys
