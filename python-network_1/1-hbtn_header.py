2486462a8757320efdba246582dda4a3bedb1394
natiman <natiabnew@gmail.com>
2023-08-10 10:58:47 -0700

    fasdasd

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,9 +5,9 @@ Sends a request to a URL and displays the value of X-Request-Id header.
 import requests
 import sys
 
-# if __name__ == "__main__":
-#     url = sys.argv[1]
-#     response = requests.get(url)
-#     x_request_id = response.headers.get("X-Request-Id")
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get("X-Request-Id")
 
-#     print(x_request_id)
+    print(x_request_id)
