86137ad07a16774015f40a32dc473d4fc1ee42de
natiman <natiabnew@gmail.com>
2023-08-22 11:45:49 -0700

    dddddddddddd

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module Docstring: Creates the states table using the State class.
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
