66367c7af63899b1d8237498b21d63817f012065
natiman <natiabnew@gmail.com>
2023-08-31 12:24:08 -0700

    dsdsdsdsdsd

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,3 +1,4 @@
+"""dmdsfjfjdsf;ksjdf;ksdjf;sdkfjksdfjkdfjdlfjdkfjdfkd;sjf;fjks;dlfksd;lfksdfk;sdlfk;sdlk"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """jijfwekfjm;dfmvdv;dmerfjgmpd;vmpvl;sovekgporgmvvmpos"""
     return "Hello HBNB!"
 
 
